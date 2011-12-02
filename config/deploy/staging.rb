73285f132e3f4b664cb771a0613608bc8571f40d
minsikzzang <minsikzzang@hotmail.com>
2011-12-02 14:44:09 +0100

    force to install google chrome framework if ie user, google ga added

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,15 +1,15 @@
-set     :user,                "deploy"
-set     :runner,              "deploy"
-set     :branch,              "staging"
-set     :rails_env,           "staging"
-set     :host,                "timejust-staging.af83.com"
-set     :default_environment, {"RAILS_ENV" => rails_env}
-set     :deploy_to,           "/var/www/app-web-alpha"
+set   :user,                "deploy"
+set   :runner,              "deploy"
+set   :branch,              "staging"
+set   :rails_env,           "staging"
+set   :host,                "staging.timejust.com"
+set   :default_environment, {"RAILS_ENV" => rails_env}
+set   :deploy_to,           "/var/www/app-web-alpha"
 
-role :app,          "staging.timejust.com"
-role :web,          "staging.timejust.com"
-role :job,          "staging.timejust.com"
-role :notification, "staging.timejust.com"
+role  :app,                 "staging.timejust.com"
+role  :web,                 "staging.timejust.com"
+role  :job,                 "staging.timejust.com"
+role  :notification,        "staging.timejust.com"
 
 # Hudsons key
 ssh_options[:keys] = %w(/home/hudson/.ssh/id_rsa)
