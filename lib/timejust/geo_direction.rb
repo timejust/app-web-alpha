3bd49cfa7718f5f4e8ff03f48a74a6f8484b5d58
minsikzzang <minsikzzang@hotmail.com>
2011-12-16 18:24:59 +0100

    missing file added

---
+++ lib/timejust/geo_direction.rb
@@ -0,0 +1,47 @@
+# encoding: UTF-8
+require 'cgi'
+require 'yajl/json_gem'
+require 'utf8proc'
+
+module Timejust
+  class GeoDirection      
+    def initialize()  
+      @@url = configatron.service.url + "/" + configatron.service.geo_direction
+      @@params = Array.new()  
+      @@results = ""  
+    end
+
+    def run()
+      hydra = Typhoeus::Hydra.hydra
+
+      json = JSON.generate(@@params)
+      req = Typhoeus::Request.new(@@url, :request => :post, :body => json)      
+      Rails.logger.info "******************* Direction #{@@url}, #{json}**************"                           
+      req.on_complete do |response|
+        if response.success? && (data = JSON.parse(response.body))
+          if data['status'] == 'ok'          
+            @@results = data['results']
+          end
+        end
+      end
+      
+      hydra.queue req      
+      hydra.run
+    end
+    
+    def results()
+      @@results
+    end
+    
+    def push_itinerary(itinerary)
+      @@params.push(itinerary)
+    end
+    
+    # class methodes
+    class << self      
+      def url
+        @@url
+      end
+    end
+  end
+end
