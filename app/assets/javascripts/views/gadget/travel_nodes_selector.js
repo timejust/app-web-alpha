e17485739052646e199b6832b9049f3f626e01e0
minsikzzang <minsikzzang@hotmail.com>
2012-01-12 20:30:06 +0100

    new address selection page added

--- app/assets/javascripts/views/gadget/travel_nodes_selector.js
+++ app/assets/javascripts/views/gadget/travel_nodes_selector.js
@@ -1,16 +1,16 @@
 App.Views.TravelNodesSelectorView = Backbone.View.extend({
   events: {
-    'submit form'                   : 'saveNewAddress',
-    'click .google_maps'            : 'openGoogleMaps',
+    'click .search_button' : 'search',
+    // : 'saveNewAddress',
+    'click .google_maps'   : 'openGoogleMaps',
+    'click .freq_address'  : 'showFreqAddress'
   },
   initialize: function(){
     // _.bindAll(this, 'waitForTravelNodes');
-    this.ip = this.options.ip;
     this.ab = eval('(' + $.cookie('ab') + ')');
     this.alias = eval('(' + $.cookie('alias') + ')');
     this.render();
-    // this.base = this.options.base;
-    // this.waitForTravelNodes();       
+    this.showGoogleMap();
   },
   getGeoAutocomplete: function(node) {
     $('#' + node).geo_autocomplete(new google.maps.Geocoder, {
@@ -18,7 +18,7 @@ App.Views.TravelNodesSelectorView = Backbone.View.extend({
   		selectFirst: false,
   		minChars: 3,
   		cacheLength: 50,
-  		width: 400,
+  		width: 384,
   		scroll: true,
   		scrollHeight: 300,
   		autoFill: true
@@ -31,119 +31,111 @@ App.Views.TravelNodesSelectorView = Backbone.View.extend({
 	  });
   }, 
   default_layout: _.template('\
-  <ul class="top"></ul>\
-  <div class="left"></div>\
-  <div class="right"></div>\
+    <div class="top"></div>\
+    <div class="main">\
+      <div class="left"></div>\
+      <div class="right"><div class="map_view"></div></div>\
+    </div>\
   '),
   top_template: _.template('\
-  <div class="logo"></div>\
-  <div class="search_box">\
-    <div class="search"><input class="keyword" name="q" maxlength="2048" size="28" id="maininput" placeholder="Search Location" /></div>\
-    <div class="search_button"><div class="button-outer_block"><div class="search_button_content"></div></div></div>\
-  </div>\
+    <div class="logo"></div>\
+    <div class="search_box">\
+      <div class="search">\
+        <input class="keyword" name="q" maxlength="2048" size="28" id="maininput" placeholder="Search Location" />\
+      </div>\
+      <div class="search_button">\
+        <div class="button-outer_block">\
+          <div class="search_button_content"></div>\
+        </div>\
+      </div>\
+    </div>\
   '),
-  // global template for form
-  form_template: _.template("\
-    <%= title %>\
-    <form width=400px action=\"#\">\
-      <%= inputs %>\
-      <div style=\"text-align:left\"><input type=\"submit\" value=\"Next\" class=\"next\"/></div>\
-    </form>\
-  "),
-  // Template for each travel node confirmation
-  input_template: _.template("\
-    <div class=\"travel_node\">\
-    <h2><%= label_type %>:</h2>\
-    <div class=\"history\">\
-    <% if (options != '') { %>\
-      <p>\
-        <select name=\"address\" class=\"selected_address\"><%= options %></select>\
-        <input type=\"button\" value=\"Maps\" class=\"google_maps\"/>\
-        <span class=\"favorite\">Add it to favorite: <input type=\"text\" name=\"alias\" placeholder=\"alias\"/></span>\
-      </p>\
-    <% } %>\
+  left_template: _.template('\
+    <div class="left-top">\
+      <div class="google_result">Google Results</div>\
+      <div class="freq_address">Frequent Addresses</div>\
     </div>\
-    <p class=\"other_address\">Or enter: <input type=\"text\" size=\"30\" id=\"<%= name %>\" name=\"address\" placeholder=\"address\"/></p>\
+    <div class="left-middle"></div>\
+    <div class="left-bottom">- (c) 2012 Timejust - </div>\
+  '),    
+  alias_result: _.template('\
+    <div class="result_block">\
+      <div class="control_block">\
+        <div class="star_symbol on"></div>\
+        <div class="title"><%=title%></div>\
+      </div>\
+      <div class="address_block" data-lat=\"<%=lat%>\" data-lng=\"<%=lng%>\">\
+        <div class="marker_<%=index%>"></div>\
+        <div id="alias" class="address"><%=address%></div>\
+        <div class="city"><%=city%></div>\
+      </div>\
     </div>\
-  "),
-  title_template: _.template("\
-    <ul class=\"travel_title\"><li class=\"prefix\">New address</li><li class=\"title_address\">selection page</li></ul>\
-  "),
-  // Template for option elements
-  option_template: _.template("<option value=\"<%= value %>\" <%= selected %> data-cached=\"1\" data-lat=\"<%=lat%>\" data-lng=\"<%=lng%>\" data-has-normalized=\"<%=has_normalized%>\" data-event-google-id=\"<%= event_id %>\"><%= label %></option>"),
-  // Return the form inputs for a given travel node
-  formInputsFor: function(travel_nodes_type, name){
-    var self = this;
-    var options = "";
-    var inputs = "";
-    // var selected_address = this.model.get(travel_nodes_type);
-    $.each(this.ab, function(i, ab) {
-      options += self.option_template( {
-        value: ab.address,
-        label: (ab.title ? ab.title + ' - ' + ab.address : ab.address),
-        // selected: (selected_address && selected_address.address == e.address ? 'selected' : ''),
-        selected: '',
-        event_id: '',
-        has_normalized: '1',
-        lat: ab.lat,
-        lng: ab.lng
-      });
-    });    
-    $.each(this.alias, function(i, a) {
-      options += self.option_template( {
-        value: a.address,
-        label: (a.title ? a.title + ' - ' + a.address : a.address),
-        // selected: (selected_address && selected_address.address == e.address ? 'selected' : ''),
-        selected: '',
-        event_id: '',
-        has_normalized: '1',
-        lat: a.lat,
-        lng: a.lng
-      });
-    });    
-    inputs += this.input_template( { 
-      label_type: this.travel_nodes_type_label(), options: options, name: name } );      
-    return inputs;
-  },
-  travel_nodes_type_label: function(){
-    if (this.ab.length > 0) {
-      return this.ab[0].title;
-    }
-    return "Address";   
-  },
+  '),
   // render the form for all travel nodes
   render: function(){
     $(this.el).html(this.default_layout);
     var top = $(this.el).find('.top');
     top.html(this.top_template);
-    this.getGeoAutocomplete('maininput');
-    /*
-    $(this.el).html(this.form_template({
-      title: this.title_template(),
-      inputs: // this.formInputsFor('previous_travel_node', 'previous_location') + 
-        this.formInputsFor('current_travel_node', 'current_location')
-        // this.formInputsFor('next_travel_node', 'next_location')
-    }));
-    this.getGeoAutocomplete('current_location');
-    // this.disableAllSearchSubmit();
-    this.$('.next').removeAttr('disabled');
-    */
+    this.getGeoAutocomplete('maininput');    
+    
+    var left = $(this.el).find('.main').find('.left');
+    left.html(this.left_template);
+    
     gadgets.window.adjustHeight();
+  },
+  search: function(e) {
+    var keyword = $('#maininput')[0].value;
+    if (keyword != "") {
+      
+    }    
+  },
+  showGoogleMap: function() {
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+      zoom: 4,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var container = $(this.el).find('.main').find('.right').find('.map_view');
+    var map = new google.maps.Map(container[0], myOptions);
+  },
+  showFreqAddress: function(e) {
+    e.preventDefault();
+    var self = this;
+    var left = $(this.el).find('.left');
+    var container = left.find('.left-middle');
+    var results = '<div class="results_block">';
+    $.each(this.alias, function(i, a) {
+      var tok = a.address.split(',');
+      var city = "";
+      $.each(tok, function(i, c) {
+        if (i > 0) {
+          city += c;
+          if (i < tok.length)
+            city += ",";
+        }          
+      });
+      // First token is address, and rest of them are city + country normally.
+      results += self.alias_result({
+        index: i,
+        title: a.title,
+        address: tok[0],
+        city: city,
+        lat: a.lat,
+        lng: a.lng
+      });
+    });
+    results += '</div>';
+    container.html(results);
   },
   // Wrap form submission to request API and confirm travel node selection
   // When done, return to the main calendar view
-  saveNewAddress: function(event){
-    // this.$('.next').attr('disabled', 'disabled');
+  saveNewAddress: function(event) {
     event.preventDefault();
     gadgets.views.requestNavigateTo('home');
     /*
     text_value = this.$('form').find('input[name="address"]').val();
-    select_value = this.$('form').find('select[name="address"]').val();
-    
-    alert(this.getTravelNodeAddress('current_travel_node'));
-    alert(this.getTravelNodeTitle('current_travel_node'));
-    alert(this.getLatitude('current_travel_node'));
-    alert(this.getLongitude('current_travel_node'));
+    select_value = this.$('form').find('select[name="address"]').val();   
     */
     /*
     if (this.getTravelNodeAddress('previous_travel_node') == 
@@ -189,60 +181,6 @@ App.Views.TravelNodesSelectorView = Backbone.View.extend({
     }
     else {
       return text_value;
-    }
-  },
-  // Return the state for a given travel node
-  getTravelNodeState: function(travel_node_type) {
-    text_value = this.$('form').find('input[name="' + travel_node_type + '\[address\]"]').val();
-    select_value = this.$('form').find('select[name="' + travel_node_type + '\[address\]"]').val();
-    if (select_value != "") {
-      return "confirmed";
-    } else {
-      return "unconfirmed";
-    }
-  },
-  // Return the address title for a given travel node, used to remember favorite addresses
-  getTravelNodeTitle: function(travel_node_type) {
-    if (this.$('form').find('input[name="' + travel_node_type + '\[title\]"]').val()){
-      return this.$('form').find('input[name="' + travel_node_type + '\[title\]"]').val();
-    } else{
-      return '';
-    }
-  },
-  getEventGoogleId: function(travel_node_type){
-    text_value = this.$('form').find('input[name="' + travel_node_type + '\[address\]"]').val();
-    select_value = this.$('form').find('select[name="' + travel_node_type + '\[address\]"]').val();
-    if (text_value == "" && select_value) {
-      return this.$('form').find('select[name="' + travel_node_type + '\[address\]"] option:selected').data('event-google-id');
-    } else {
-      return "";
-    }
-  },
-  getLatitude: function(travel_node_type) {
-    text_value = this.$('form').find('input[name="' + travel_node_type + '\[address\]"]').val();
-    select_value = this.$('form').find('select[name="' + travel_node_type + '\[address\]"]').val();
-    if (select_value) {
-      return this.$('form').find('select[name="' + travel_node_type + '\[address\]"] option:selected').data('lat');
-    } else {
-      return "";
-    }
-  },
-  getLongitude: function(travel_node_type) {
-    text_value = this.$('form').find('input[name="' + travel_node_type + '\[address\]"]').val();
-    select_value = this.$('form').find('select[name="' + travel_node_type + '\[address\]"]').val();
-    if (select_value) {
-      return this.$('form').find('select[name="' + travel_node_type + '\[address\]"] option:selected').data('lng');
-    } else {
-      return "";
-    }
-  },
-  hasNormalized: function(travel_node_type) {
-    text_value = this.$('form').find('input[name="' + travel_node_type + '\[address\]"]').val();
-    select_value = this.$('form').find('select[name="' + travel_node_type + '\[address\]"]').val();
-    if (select_value) {
-      return this.$('form').find('select[name="' + travel_node_type + '\[address\]"] option:selected').data('has-normalized');
-    } else {
-      return "";
     }
   },
   // Close travel node selector view and show home view
