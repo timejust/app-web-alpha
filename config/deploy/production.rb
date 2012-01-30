384e3f95422ee4195297cb09f2ce7d3ceb4f35f3
minsikzzang <minsikzzang@hotmail.com>
2012-01-30 19:42:30 +0100

    some production configuration added

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,17 @@
-set     :branch,              "develop"
-set     :rails_env,           :production
-set     :host,                "timejust.com"
-set     :default_environment, {"RAILS_ENV" => rails_env}
-set     :deploy_to,           "/var/www/#{user}/#{rails_env}/timejust-api"
-server  "#{user}@#{host}", :app, :web, :db, :primary => true
+set   :user,                "deploy"
+set   :runner,              "deploy"
+set   :branch,              "production"
+set   :rails_env,           "production"
+set   :host,                "beta.timejust.com"
+set   :default_environment, {"RAILS_ENV" => rails_env}
+set   :deploy_to,           "/var/www/app-web-alpha"
+
+role  :app,                 "beta.timejust.com"
+role  :web,                 "beta.timejust.com"
+role  :job,                 "beta.timejust.com"
+role  :notification,        "beta.timejust.com"
+
+# Hudsons key
+ssh_options[:keys] = %w(/home/hudson/.ssh/id_rsa)
+
+default_environment["PATH"] = "$PATH:/usr/local/lib/ruby/gems/1.9.1/bin/"
